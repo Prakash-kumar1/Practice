902797a3e5d39ab465f525bf3edeef0ced28c80c
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-02 13:07:58 +0530

    using loop

--- 02-july.js
+++ 02-july.js
@@ -1,16 +1,25 @@
 // Recursion -->>
 
-function add(n){
-    let sum = n+10 ;
-   console.log(sum , n);
-   add(sum)
-}
-add(10)
+// function add(n){
+//     let sum = n+10 ;
+//    console.log(sum , n);
+//    add(sum)
+// }
+// add(10)
 
 // Any Recursion problem solved by Loops.
 
-function countdown(n){
-    for(let i=1; i>0; i++){
+// Using For Loop  
+
+function countdown(n){                                                                               
+    for(let i=n; i>0; i--){                                       
         console.log(i) ;
     }
+    console.log('ended')
 }
+countdown(10)
+
+
+// Using Recursion
+
+function countdown(n)
