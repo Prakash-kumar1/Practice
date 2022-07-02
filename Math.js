9f1f0f74d1fcd22a8f3ec5a98f97e2123049c32c
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-02 12:00:03 +0530

    exporting this file in 01-july

---
+++ Math.js
@@ -0,0 +1,11 @@
+function getsum(n1,n2){
+    return n1+n2
+}
+module.exports = getsum
+
+// getsubtract override the getsum function. the value of getsum not print in class.js Terminal.
+
+function getsubtract(n1,n2){
+    return n1-n2
+}
+module.exports = getsubtract
