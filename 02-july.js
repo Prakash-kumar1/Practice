c646b1e53f7247f5982d2bc5c6ad039e90718f98
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-03 12:13:31 +0530

    updating

--- 02-july.js
+++ 02-july.js
@@ -1,11 +1,11 @@
 // Recursion -->>
 
-function add(n){
-    let sum = n+10 ;
-   console.log(sum , n);
-   add(sum)
-}
-add(10)
+// function add(n){
+//     let sum = n+10 ;
+//    console.log(sum , n);
+//    add(sum)
+// }
+// add(10)
 
 // Any Recursion problem solved by Loops.
 
@@ -13,28 +13,28 @@ add(10)
 
 // Using For Loop  
 
-function countdown(n){                                                                               
-    for(let i=n; i>0; i--){                                       
-        console.log(i) ;
-    }
-    console.log('ended')
-}
-countdown(10)
+// function countdown(n){                                                                               
+//     for(let i=n; i>0; i--){                                       
+//         console.log(i) ;
+//     }
+//     console.log('ended')
+// }
+// countdown(10)
 
 
 // Using Recursion
 
-function countdown(n){
-//base condition
-if(n <= 0){
-    console.log('ended');
-    return ;
-}
-console.log(n) ;
-countdown(n-1)
-}
-countdown(10)
-countdown(5)
+// function countdown(n){
+// //base condition
+// if(n <= 0){
+//     console.log('ended');
+//     return ;
+// }
+// console.log(n) ;
+// countdown(n-1)
+// }
+// countdown(10)
+// countdown(5)
 
 // xxxxxxxxxxxxxxxx ------->> Example 1 Completed   <<--------xxxxxxxxxxxxxxxxx ------->>
 
@@ -44,26 +44,27 @@ countdown(5)
 
 // Using For Loop 
 
-function sumofRange(n) {
-    let total = 0;
-    for(let i=n; i>0; i--){
-        total += i ;
-    }
-    return total ;
-}
-console.log(sumofRange(10))
+// function sumofRange(n) {
+//     let total = 0;
+//     for(let i=n; i>0; i--){
+//         total += i ;
+//     }
+//     return total ;
+// }
+// console.log(sumofRange(10))
 
 
 // Using Recursion
 
-function sumofRange(n , total) {
-    if(n <= 0){
-        return total
-    }
-    total += n
-    return sumofRange(n-1 , total)
-}
-console.log(sumofRange(10 , 0))
+// function sumofRange(n , total) {
+//     if(n <= 0){
+//         return total
+//     }
+//     total += n
+//     return sumofRange(n-1 , total)
+// }
+// console.log(sumofRange(10 , 0))
 
 
 // xxxxxxxxxxxxxxxx ------->> Example - 2 Completed   <<--------xxxxxxxxxxxxxxxxx ------->>
+  
