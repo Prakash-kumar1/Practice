516f576332683997255e1861c378180d7d89f8a9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-13 18:52:28 +0530

    Basic Concept Of Binary Search.

--- Searching.js
+++ Searching.js
@@ -1,6 +1,6 @@
 // Searching Algorithms
 
-// Linear Search
+// // Linear Search
 
 let nums = [11, 23, 69, 45, 36, 87]
 let target =  45
@@ -12,3 +12,26 @@ function isPresent(nums , target){
         return false ;
     }
 console.log(isPresent(nums , target));
+
+
+// Binary Search-: First sort the Array
+
+let nus = [11, 23, 36, 45, 69, 87, 99]
+target = 11
+
+let start = 0;
+let end = nus.length-1 ;
+
+while(start <= end){
+    let middle = Math.floor(start+end / 2);
+    if(nus[middle] == target){
+        console.log('found at' , middle)
+        start += 1 ;
+        end -= 1;
+        break ;
+    }else if(nus[middle] < target){
+        start = middle + 1;
+    }else{
+        end = middle - 1 ;
+    }
+}
