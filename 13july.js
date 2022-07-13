85d5e0b39e55e8d50ca1e39f44e0318798500f70
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-13 12:25:23 +0530

    2D - Array

---
+++ 13july.js
@@ -0,0 +1,21 @@
+// 2D - Array : Revise on your own . 
+
+let nums = [2, 3, 5, 6, 7, 8]
+let target = 9
+
+let leftindex = 0
+let rightindex = nums.length - 1
+
+while(leftindex < rightindex){
+    if(nums[leftindex] + nums[rightindex] == target){
+        console.log(nums[leftindex] , nums[rightindex]);
+        leftindex += 1 ;
+        rightindex -= 1 ;
+    }else if(nums[leftindex] + nums[rightindex]  >  target){
+        rightindex -= 1 ;
+    }else if(nums[leftindex] + nums[rightindex]  <  target){
+        leftindex += 1 ;
+    }
+}
+
+
