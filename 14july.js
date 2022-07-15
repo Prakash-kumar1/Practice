e37c33c948b8fc129b48341c300c5c11b336bf18
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-15 16:43:30 +0530

    How to find a word in Paragraph?

--- 14july.js
+++ 14july.js
@@ -12,29 +12,22 @@
 //  NewArray = [...array]
 //  console.log(NewArray)
 
-   
-
-
-
-
-
-
 
 // // How to find a word in Paragraph? - : in array we used includes method but in string we used Flag method
 
-// let news = 'india won the world cup. India won the world cup after 2011. india is a bad country, and also India is a poor country'
-// let target = 'won'
+let news = 'india won the world cup. India won the world cup after 2011. india is a bad country, and also India is a poor country'
+let target = 'won'
 
-// // first convert into Array using split 
-// let newsArray = news.split(' ')
+// first convert into Array using split 
+let newsArray = news.split(' ')
 
-// let isPresent = false ;
-// for(let i=0; i<newsArray.length; i++){
-//     if(newsArray[i] === target){
-//         isPresent = true ;
-//     }
-// }
-// console.log(isPresent)
+let isPresent = false ;
+for(let i=0; i<newsArray.length; i++){
+    if(newsArray[i] === target){
+        isPresent = true ;
+    }
+}
+console.log(isPresent)
 
 
 // // // Using endswith method-:
