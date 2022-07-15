d2a9dfdfa915f4417146a3a6761a10b693344d73
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-15 16:48:07 +0530

    Checking email

--- 14july.js
+++ 14july.js
@@ -13,33 +13,33 @@
 //  console.log(NewArray)
 
 
-// // How to find a word in Paragraph? - : in array we used includes method but in string we used Flag method
+// // // How to find a word in Paragraph? - : in array we used includes method but in string we used Flag method
 
-let news = 'india won the world cup. India won the world cup after 2011. india is a bad country, and also India is a poor country'
-let target = 'won'
+// let news = 'india won the world cup. India won the world cup after 2011. india is a bad country, and also India is a poor country'
+// let target = 'won'
 
-// first convert into Array using split 
-let newsArray = news.split(' ')
+// // first convert string into Array using split  and {by using join method we convert Array into String.}
+// let newsArray = news.split(' ')
 
-let isPresent = false ;
-for(let i=0; i<newsArray.length; i++){
-    if(newsArray[i] === target){
-        isPresent = true ;
-    }
-}
-console.log(isPresent)
-
-
-// // // Using endswith method-:
-
-// // Example-1:  
-// let email = 'kumarsanju4881@gmail.com'
-
-// if(email.endsWith('m')){
-//     console.log('valid email')
-// }else{
-//     console.log('Invalid email')
+// let isPresent = false ;
+// for(let i=0; i<newsArray.length; i++){
+//     if(newsArray[i] === target){
+//         isPresent = true ;
+//     }
 // }
+// console.log(isPresent)
+
+
+// // // // Using endswith method-:
+
+// // // Example-1:  
+let email = 'kumarsanju4881@gmail.com'
+
+if(email.endsWith('m')){
+    console.log('valid email')
+}else{
+    console.log('Invalid email')
+}
 
 // // // // Using startsswith method-:
 // let mobilenumber = '+91 8127874881'
