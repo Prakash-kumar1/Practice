f2ff4bbc06135ae9054b7baccc42cd01e024593d
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-16 19:52:01 +0530

    solving above question using Callback Function.

--- 16july.js
+++ 16july.js
@@ -3,11 +3,13 @@
 let chars = ["a", "b", "a", "c", "b"]
 let uniqueArray = []
 for(let i=0; i< chars.length; i++){
-    if(!uniqueArray.includes(chars[i])){
+    if(! uniqueArray.includes(chars[i])){
         uniqueArray.push(chars[i]) ;
     }
 }
 console.log(uniqueArray) ;
+
+//  solving above question using Callback Function.
 
 
 
