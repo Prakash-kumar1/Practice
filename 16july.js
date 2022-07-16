fdfb6c501d1ecd8cf7f3b00dc62b8ff1a16759df
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-16 22:31:40 +0530

    Printing matrix Pattern

--- 16july.js
+++ 16july.js
@@ -81,13 +81,54 @@
 // console.log(isValid("{[]}")) ;
 
 
+// // Question no.-5 : Rotate the number by 1 character ??
+// let string = 'Aircampus' ;
+// let rotatingString = string.slice(0,2) ;
+// let slicedString = string.slice(1) ;
+// console.log(`'${slicedString}''${rotatingString}'`) ;
 
-// Question no.-5 : Rotate the number by 1 character ??
-let string = 'Aircampus' ;
-let rotatingString = string.slice(0,2) ;
-let slicedString = string.slice(1) ;
-console.log(`'${slicedString}' '${rotatingString}'`) ;
-
-
-
-
+// Question no.-6 : print 1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10
+  
+let matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
+  ];
+  
+  let top = 0,bottom = 3, left = 0, right = 3, direction = 0;
+  
+    while(left <= right && top<=bottom) {
+      if(direction == 0) {
+          for(let i=left; i<=right; i++) {
+              console.log(matrix[top][i]);
+          }
+          top += 1;
+          direction = 1;
+      }
+  
+      if(direction == 1) {
+          for(let i=top; i<=bottom; i++) {
+              console.log(matrix[i][right]);
+          }
+          right -= 1;
+          direction = 2;
+      }
+  
+      if(direction == 2) {
+          for(let i=right; i>= left; i--) {
+              console.log(matrix[bottom][i]);
+          }
+          bottom -= 1;
+          direction = 3;
+      }
+  
+      if(direction == 3) {
+          for(let i=bottom; i>=top; i--) {
+              console.log(matrix[i][left]);
+          }
+          left += 1;
+          direction = 0
+      }
+    }
+  
