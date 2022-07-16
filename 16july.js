b33cac6f7fa7259136a9407222d24450751d1c5a
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-16 20:39:13 +0530

    Update 16july.js

--- 16july.js
+++ 16july.js
@@ -26,22 +26,23 @@
 // }
 
 
-// Question No.-3: find largest numbers ?
+// // Question No.-3: find largest numbers ?
 
-let nums = [11, 9, 4, 17, 25, 33, 19]
-// if you write b-a it will give Descending Order.
-nums.sort((a,b) => a-b)
-console.log(nums) ;
-console.log(nums[nums.length - 1]) ;
+// let nums = [11, 9, 4, 17, 25, 33, 19]
+// // if you write b-a it will give Descending Order.
+// nums.sort((a,b) => a-b)
+// console.log(nums) ;
+// console.log(nums[nums.length - 1]) ;
 
-// using another method find largest number
+// // using another method find largest number 
 
-function getLargest(nums , position){
-    nums.sort((a,b) => b - a) ;
-    console.log(nums);
-    return nums[position - 1] ;
-}
-console.log(getLargest(nums , 1))
+// function getLargest(nums , position){
+//     nums.sort((a,b) => b - a) ;
+//     console.log(nums);
+//     return nums[position - 1] ;
+// }
+// // if you want IInd largest or IIIrd largest just put Position.
+// console.log(getLargest(nums , 1))
 
 
 
