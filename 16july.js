e8f2d4b0b1a7936e79e83b7751f37d02c31b5bca
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-16 19:49:31 +0530

    find unique characters ?

--- 16july.js
+++ 16july.js
@@ -1,8 +1,13 @@
 // Question No.-1: find unique characters ?
 
 let chars = ["a", "b", "a", "c", "b"]
-
-
+let uniqueArray = []
+for(let i=0; i< chars.length; i++){
+    if(!uniqueArray.includes(chars[i])){
+        uniqueArray.push(chars[i]) ;
+    }
+}
+console.log(uniqueArray) ;
 
 
 
