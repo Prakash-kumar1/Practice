5bd519aaea0e4adca2a1c7fb85aa3c83943f959d
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-19 11:33:48 +0530

    Update 18-July.js

--- 18-July.js
+++ 18-July.js
@@ -1,1 +1,13 @@
 // Objects -: 
+
+Circle = {
+    radius : 1,
+    location: {
+        x : 2,
+        y : 2,
+    },
+};
+console.log(Circle["location"] , Circle["location"]["x"] , Circle["location"]["y"])
+// OR But prefer Array property donot use Dot Operator
+console.log(Circle.location , Circle.location.x , Circle.location.y)
+
