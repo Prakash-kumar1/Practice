4fcecb8edbd26a474f3e0dfab880b764f6ad8e2f
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-19 11:38:31 +0530

    we can also use Function inside  of Objects.

--- 18-July.js
+++ 18-July.js
@@ -11,3 +11,20 @@ console.log(Circle["location"] , Circle["location"]["x"] , Circle["location"]["y
 // OR But prefer Array property donot use Dot Operator
 console.log(Circle.location , Circle.location.x , Circle.location.y)
 
+// we can also use Function inside  of Objects.
+
+Circle = {
+    radius : 1,
+    location: {
+        x : 2,
+        y : 2,
+    },
+    draw: function() {
+        console.log("Drawing Circle") ;
+    },
+    paint : function() {
+        console.log("Painting the circle Radius")
+    },
+};
+console.log(Circle.draw())
+console.log(Circle.paint())
