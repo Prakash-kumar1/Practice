f95ee057a5177bfd49d97cc516b9a6d9aa7f27c9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-21 15:44:49 +0530

    Update 20-July.js

--- 20-July.js
+++ 20-July.js
@@ -1,116 +1,116 @@
-// function declaration --> using function keyword
+// // function declaration --> using function keyword
 
-function log(){
-    console.log("Declaration logging") ;
-}
-log() ;
-
-
-// Function Expression ( " Anonymous Function " )
-
-let printing = function(){
-    console.log("Expression logging") ;
-};
-
-printing() ;
+// function log(){
+//     console.log("Declaration logging") ;
+// }
+// log() ;
 
 
-// Named Function Expression (Not  useful)
+// // Function Expression ( " Anonymous Function " )
 
-let name = function Prakash (){
-    console.log("Named Expression") ;
-};
-name() ;
+// let printing = function(){
+//     console.log("Expression logging") ;
+// };
 
-
-//  Hoisting : Calling Function Before Declaration . 
-
-pets();
-
-function pets(){
-    console.log("Pets in the home") ;
-} ;
-
-// Function Expression can't be Hoisted it will give  -->  Error   because  we can't access before  Intialization
+// printing() ;
 
 
-//  Arguments  in Function 
+// // Named Function Expression (Not  useful)
 
-function add(num1 , num2 ){
-    // console.log(arguments) ;
-    return (num1+num2);
-}
-
-console.log(add(5,10))
-
-//  in Javascript if we don't pass any value it will considered as "Undefined" and if we pass extra value it will considered as "Ignore"
-
-function add(num1 , num2){
-    return (num1+num2);
-}
-// Extra values ignored
-console.log(add(15,10,34))
+// let name = function Prakash (){
+//     console.log("Named Expression") ;
+// };
+// name() ;
 
 
+// //  Hoisting : Calling Function Before Declaration . 
 
-function add(num1 , num2,num3,num4){
-    // num3 , num4 are undefined
-    console.log(num1 , num2,num3,num4)
-    // it will give None
-    console.log(num1 + num4)
-        return (num1+num2);
-    }
+// pets();
+
+// function pets(){
+//     console.log("Pets in the home") ;
+// } ;
+
+// // Function Expression can't be Hoisted it will give  -->  Error   because  we can't access before  Intialization
+
+
+// //  Arguments  in Function 
+
+// function add(num1 , num2 ){
+//     // console.log(arguments) ;
+//     return (num1+num2);
+// }
+
+// console.log(add(5,10))
+
+// //  in Javascript if we don't pass any value it will considered as "Undefined" and if we pass extra value it will considered as "Ignore"
+
+// function add(num1 , num2){
+//     return (num1+num2);
+// }
+// // Extra values ignored
+// console.log(add(15,10,34))
+
+
+
+// function add(num1 , num2,num3,num4){
+//     // num3 , num4 are undefined
+//     console.log(num1 , num2,num3,num4)
+//     // it will give None
+//     console.log(num1 + num4)
+//         return (num1+num2);
+//     }
     
-    console.log(add(10,34)) ;
+//     console.log(add(10,34)) ;
 
 
-// // Rest operator(...) : whatever value passing it will converting into Array.
+// // // Rest operator(...) : whatever value passing it will converting into Array.
 
-function shoppingCart(...prices){
-    console.log(prices) ;
-    let total = 0;
-    for(let i=0; i<prices.length; i++){
-        total += prices[i] ;
-    }
-    console.log(total) ;
-}
-shoppingCart(100,150,50,300)
+// function shoppingCart(...prices){
+//     console.log(prices) ;
+//     let total = 0;
+//     for(let i=0; i<prices.length; i++){
+//         total += prices[i] ;
+//     }
+//     console.log(total) ;
+// }
+// shoppingCart(100,150,50,300)
 
-//  whenever we use Rest operator , always use in last as a parameter . if we write anything after rest operator it will gives error
+// //  whenever we use Rest operator , always use in last as a parameter . if we write anything after rest operator it will gives error
 
-function shoppingCart(discount , ...prices){
-console.log(discount , prices )
-    let total = 0;
-    for(let i=0; i<prices.length; i++){
-        total += prices[i] ;
-    }
-    console.log(total) ;
-}
-shoppingCart(10,50,50,30)
+// function shoppingCart(discount , ...prices){
+// console.log(discount , prices )
+//     let total = 0;
+//     for(let i=0; i<prices.length; i++){
+//         total += prices[i] ;
+//     }
+//     console.log(total) ;
+// }
+// shoppingCart(10,50,50,30)
 
 
-function shoppingCart(discount ,coupan , ...prices){
-    // console.log(discount , prices , coupan)
-        let total = 0;
-        for(let i=0; i<prices.length; i++){
-            total += prices[i] ;
-            total = (total * discount) / 100 - coupan ;
-        }
-        return total ;
-    }
-    console.log(shoppingCart(5,60,150,300,100)) ;
+// function shoppingCart(discount ,coupan , ...prices){
+//     // console.log(discount , prices , coupan)
+//         let total = 0;
+//         for(let i=0; i<prices.length; i++){
+//             total += prices[i] ;
+//             total = (total * discount) / 100 - coupan ;
+//         }
+//         return total ;
+//     }
+//     console.log(shoppingCart(5,60,150,300,100)) ;
 
 
 // // Function Inside Object
 
-// let person = {
-//     fname : "Prakash" ,
-//     lname : "kumar" ,
-//     fullname() {
-//         return `${person.fname} ${person.lname}` ;
-//     },
-// }; 
-// console.log(person.fullname()) ;
+let person = {
+    fname : "Prakash" ,
+    lname : "kumar" ,
+    fullname() {
+        return `${person.fname} ${person.lname}` ;
+    },
+}; 
+console.log(person.fullname()) ;
 
 
 // // Using get and set 
