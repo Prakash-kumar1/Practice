202d7d36da6959d1bb167b74a20780f4cae3bcb0
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-21 21:49:42 +0530

    Concepts  &  problem on Recursion

--- 21-July.js
+++ 21-July.js
@@ -1,15 +1,15 @@
 // Destructuring :
 
-// let nums = [1,2,3,4,5] ;
-// Ist value copy into num1 & IInd value copy into num2
+let nums = [1,2,3,4,5] ;
+// // Ist value copy into num1 & IInd value copy into num2
 // let [num1 , num2] = nums ;
 // console.log(num1 , num2)
 
 
 // for getting all elements use Rest Operator
 
-//  let [num1 , num2, ...num3] = nums ;
-// console.log(num1 , num2 , num3)
+let [num1 , num2, ...num3] = nums ;
+console.log(num1 , num2 , num3)
 
 
 // if we pass extra parameter , it will gives Defined
@@ -21,24 +21,77 @@
 
 // // Object Destructuring
 
-// let obj = {
-//     name : 'satya',
-//     Class : "Frontend" ,
-//    age: 67,
-//     profession: 'developer',
-// };
+let obj = {
+    name : 'satya',
+    Class : "Frontend" ,
+   age: 67,
+    profession: 'developer',
+};
 
-// let {name , age} = obj ;
-// console.log(name , age)
+let {name , age} = obj ;
+console.log(name , age)
 
-// // Using rest operator it will give all elements
-// let {Class , ...rest} = obj ;
-// console.log(obj)
+// Using rest operator it will give all elements
+let {Class , ...rest} = obj ;
+console.log(obj)
 
-// Recursion : - Calling function again and again
+// // Recursion : - Calling function again and again. it is based on "Divide & Conquere approaches" . Recursion are mostly use tree data structure , graph algorithm etc.
 
-function add() {
-    console.log("calling");
-    add();
+// // Example-1 : it calling itself continous 
+// function add() {
+//     console.log("calling");
+//     add();
+// }
+// add();
+
+
+// // Question no-1 find factorial using Recursion ?
+
+function getFactorial(num){
+  if(num == 1){
+    return 1 ;
+  }
+  return num * getFactorial(num-1) ;
 }
-add();
+console.log(getFactorial(5));
+
+// // JavaScript has limit of 260 after that it will gives Infinity
+// console.log(getFactorial(265));
+
+
+
+// //  Question no-2 find sum of all elements in array using Recursion ?
+
+
+let numbers = [1,2,3,4,5] ;
+
+function findSum(arr) {
+    if(arr.length === 1){
+        return arr[0] ;
+    } 
+
+// if we pop , it will give same value .
+    let firstElement = arr.shift() ;
+
+   return firstElement + findSum(arr) ;
+}
+
+console.log(findSum(numbers)) ;
+console.log(findSum([1,2])) ;
+console.log(findSum([1,21,100])) ;
+
+
+
+// //  Question no-2 find sum-range between two numbers using Recursion ?
+
+function getSumRange(first , last){
+    if(first === last){
+        return 0 ;
+    }
+    return first + getSumRange(first+1 , last)
+}
+console.log(getSumRange(5,7)) ;
+console.log(getSumRange(15,57)) ;
+
+
+
