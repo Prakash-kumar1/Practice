2b2c38d856c4fabcda22f86ebcd2ef02b6ea8075
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-22 19:44:35 +0530

    Update 22-July.js

--- 22-July.js
+++ 22-July.js
@@ -1,2 +1,24 @@
 // Sorting Algorithms
 
+////  Question no-2 find Power of a number using Recursion ?
+
+
+// function getPower(base , exponential){
+//     let total = 1 ;
+// for(let i=0; i<exponential; i++){
+//     total *= base ;
+// }
+// return total ;
+// }
+// console.log(getPower(2,4))
+
+
+function getPower(base , exponential){
+    if(exponential == 0) return 1 ;
+    let total = 1 ;
+for(let i=0; i<exponential; i++){
+    total *= base ;
+}
+return total ;
+}
+console.log(getPower(2,3))
