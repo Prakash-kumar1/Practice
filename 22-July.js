7315a1a819ab19c41757d5613d5c8fe91e5e07bf
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-22 20:18:38 +0530

    Update 22-July.js

--- 22-July.js
+++ 22-July.js
@@ -1,49 +1,59 @@
 // ////  Question no-1 find Power of a number using Function ?
 
 
-// function getPower(base , exponential){
-//     let total = 1 ;
-// for(let i=0; i<exponential; i++){
-//     total *= base ;
-// }
-// return total ;
-// }
-// console.log(getPower(2,4))
+function getPower(base , exponential){
+    let total = 1 ;
+for(let i=0; i<exponential; i++){
+    total *= base ;
+}
+return total ;
+}
+console.log(getPower(2,4))
 
 
 // // Using Base Condition (line-19) & Error condition (line-18)
 
 
-// function getPower(base , exponential){
-//     if(exponential < 0) return -1 ;
-//     if(exponential == 0) return 1 ;
-//     let total = 1 ;
-// for(let i=0; i<exponential; i++){
-//     total *= base ;
-// }
-// return total ;
-// }
-// console.log(getPower(2,-1)) 
+function getPower(base , exponential){
+    if(exponential < 0) return -1 ;
+    if(exponential == 0) return 1 ;
+    let total = 1 ;
+for(let i=0; i<exponential; i++){
+    total *= base ;
+}
+return total ;
+}
+console.log(getPower(2,-1)) 
 
 
 // // Question no-2  find Power of a number using Recursion ?
 
-// function Power(base , exponent){
-//         if(exponent === 0) return 1 ;
-//     return  base * Power(base , exponent - 1) ;
-//     }
-//     console.log(Power(2,5));
+function Power(base , exponent){
+        if(exponent === 0) return 1 ;
+    return  base * Power(base , exponent - 1) ;
+    }
+    console.log(Power(2,5));
 
 
 
 // // Question no-3 Write a program to make nth dimensional array into 1-D Array not use "Flat()" ??
 
-let arr =  [[1, 2, [3, 4]]] ;
+
+let arr =  [[1, 2, 3, 4]] ;
 
 function flatter(arr){
   let flatterArray = [] ;
-
-
+if(Array.isArray(arr) === false){
+    return arr ;
+}
+for(let i=0; i<arr.length;i++){
+    if(Array.isArray(arr[i])){
+        return flatter(arr[i]) ;
+    }else{
+        flatterArray.push(arr[i]) ;
+    }
+}
+   return flatterArray ;
 }
 console.log(flatter(arr)) ;
 
