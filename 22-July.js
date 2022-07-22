9233a7a4d1925e3fd6d4e3530cb191acde69b4ca
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-22 19:55:02 +0530

    solving Recursion Problems

--- 22-July.js
+++ 22-July.js
@@ -1,39 +1,35 @@
-
-
 ////  Question no-1 find Power of a number using Function ?
 
 
-// function getPower(base , exponential){
-//     let total = 1 ;
-// for(let i=0; i<exponential; i++){
-//     total *= base ;
-// }
-// return total ;
-// }
-// console.log(getPower(2,4))
+function getPower(base , exponential){
+    let total = 1 ;
+for(let i=0; i<exponential; i++){
+    total *= base ;
+}
+return total ;
+}
+console.log(getPower(2,4))
 
-// // Using Base Condition (line-19) & Error condition (line-18)
 
-// function getPower(base , exponential){
-//     if(exponential < 0) return -1 ;
-//     if(exponential == 0) return 1 ;
-//     let total = 1 ;
-// for(let i=0; i<exponential; i++){
-//     total *= base ;
-// }
-// return total ;
-// }
-// console.log(getPower(2,3));
-// console.log(getPower(2,-1))
+// Using Base Condition (line-19) & Error condition (line-18)
 
-// 
+
+function getPower(base , exponential){
+    if(exponential < 0) return -1 ;
+    if(exponential == 0) return 1 ;
+    let total = 1 ;
+for(let i=0; i<exponential; i++){
+    total *= base ;
+}
+return total ;
+}
+console.log(getPower(2,-1)) 
 
 
 // Question no-2  find Power of a number using Recursion ?
-
 
 function Power(base , exponent){
         if(exponent === 0) return 1 ;
     return  base * Power(base , exponent - 1) ;
     }
-    console.log(Power(2,4));
+    console.log(Power(2,5));
