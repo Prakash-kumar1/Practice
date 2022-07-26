b39245e8157c535b1bb3e6241807180cea4684fe
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-26 20:42:54 +0530

    Searching Methods  &  String  Methods

--- Searching.js
+++ Searching.js
@@ -16,25 +16,25 @@ console.log(isPresent(nums , target));
 
 // // Binary Search-: First sort the Array
 
-// let nus = [11, 23, 36, 45, 69, 87, 99]
-// target = 11
+let nus = [11, 23, 36, 45, 69, 87, 99]
+target = 11
 
-// let start = 0;
-// let end = nus.length-1 ;
+let start = 0;
+let end = nus.length-1 ;
 
-// while(start <= end){
-//     let middle = Math.floor(start+end / 2);
-//     if(nus[middle] == target){
-//         console.log('found at' , middle)
-//         start += 1 ;
-//         end -= 1;
-//         break ;
-//     }else if(nus[middle] < target){
-//         start = middle + 1;
-//     }else{
-//         end = middle - 1 ;
-//     }
-// }
+while(start <= end){
+    let middle = Math.floor(start+end / 2);
+    if(nus[middle] == target){
+        console.log('found at' , middle)
+        start += 1 ;
+        end -= 1;
+        break ;
+    }else if(nus[middle] < target){
+        start = middle + 1;
+    }else{
+        end = middle - 1 ;
+    }
+}
 
 // // // Strings -: This is also a  Spread Operator.
 let fname = 'Prakash'
