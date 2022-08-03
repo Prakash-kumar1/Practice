e0e119a4a5c4df8f0ec6003c75255274ec68e175
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-03 21:18:55 +0530

    getElementsByTagName

--- DOM_Sessions/app.js
+++ DOM_Sessions/app.js
@@ -18,13 +18,36 @@ document.querySelector("h1").innerText = "Good Bye ! class is over"
 console.log(document.querySelector(".IInd").innerText)    // Welcome Prakash
 
 
+// // Accessing ul li  values
 
+let list = document.querySelectorAll('ul li') ;
+console.log(list) ;                            // NodeList(8) [li, li, li, li, li, li, li, li]
 
+list = document.querySelectorAll('ul li')[0].innerText ;
+console.log(list) ;       // 1
 
+list = document.querySelectorAll('ul li')[2].innerText
+console.log(list) ;            // 3
 
+list = document.querySelectorAll('ul li')[5].innerText
+console.log(list) ;  
 
+list = document.querySelectorAll('ul li')[7].innerText
+console.log(list) ;  
 
+// // Using For loop  for printing values and also changing values
 
+let change = document.querySelectorAll('ul li');
+for (let i = 0; i < change.length; i++) {
+    // console.log(i);                                    // it give index values of list
+console.log(change[i].innerText) ;                               // it will give all values
+
+//  changing "vasanth"  to  "Shivansh Rawat"
+if (change[i].innerText === "vasanth") {
+    change[i].innerText = " Shivansh Rawat"    
+}
+    console.log(change[i].innerText) ;                         // it change see on console
+}
 
 
 
