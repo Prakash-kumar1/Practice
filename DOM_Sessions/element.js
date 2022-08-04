fd2ea46dbfe528070aac9c55c400815036234308
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-04 12:01:21 +0530

    capturing

--- DOM_Sessions/element.js
+++ DOM_Sessions/element.js
@@ -6,43 +6,71 @@ const Child = document.querySelector(".child") ;
 // console.log(parent) ;
 // console.log(Child) ;
 
-// In Javascript , everything is an event like hovering , clicking etc
+// // In Javascript , everything is an event like hovering , clicking etc
 
 
-// Event Bubbling: whwnever we click on child it will print 'child, parent & grandParent' and when we click on Parent it will print 'parent & grandParent'  and again and when we click on grandParent it will print ' grandParent' .clicking check last one is Event or not
+// // Event Bubbling: whwnever we click on child it will print 'child, parent & grandParent' and when we click on Parent it will print 'parent & grandParent'  and again and when we click on grandParent it will print ' grandParent' .clicking check last one is Event or not
+
+// grandParent.addEventListener("click" , () => {
+//     console.log("grandParent");                                      
+// }) ;
+
+// Parent.addEventListener("click" , () => {
+//     console.log("Parent") ;
+// });
+
+// Child.addEventListener("click" , () => {
+//     console.log("Child");                                       
+// }) ;
+
+
+
+// document.querySelector("body").addEventListener("click" , () => {
+//     console.log("body") ;
+// }) ;
+
+// document.querySelector("html").addEventListener("click" , () => {
+//     console.log("html") ;
+// }) ;
+
+// document.addEventListener("click" , () => {
+//     console.log("document");                                       
+// }) ;
+
+// // output:-  child
+// //           parent
+// //           grandParent
+// //           body
+// //           html
+// //          document
+// // because when we click on body it will check that html is an event or not , if it is an Event then print ,if it is not an Event then don't print . similarly , for document , it will check
+
+
+// event capturing
 
 grandParent.addEventListener("click" , () => {
-    console.log("grandParent");                                      
+    console.log("grandParent Bubbling");                                      
 }) ;
 
-
+Parent.addEventListener("click" , () => {
+    console.log("Parent Bubbling") ;
+});
 
 Child.addEventListener("click" , () => {
-    console.log("Child");                                       
+    console.log("Child Bubbling");                                       
 }) ;
 
+grandParent.addEventListener("click" , () => {
+    console.log("grandParent Capturing");                                      
+},{capture : true}) ;
 
+Parent.addEventListener("click" , () => {
+    console.log("Parent Capturing") ;
+},{capture : true});
 
-document.querySelector("body").addEventListener("click" , () => {
-    console.log("body") ;
-}) ;
-
-document.querySelector("html").addEventListener("click" , () => {
-    console.log("html") ;
-}) ;
-
-document.addEventListener("click" , () => {
-    console.log("document");                                       
-}) ;
-
-// output:- body
-//         html
-//          document
-// because when we click on body it will check that html is an event or not , if it is an Event then print ,if it is not an Event then don't print . similarly , for document , it will check
-
-
-
-
+Child.addEventListener("click" , () => {
+    console.log("Child Capturing");                                       
+},{capture : true}) ;
 
 
 
