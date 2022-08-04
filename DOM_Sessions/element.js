6fe9b3a4991ae50981b63c87d9c831838e1044e7
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-04 11:48:43 +0530

    fnh

--- DOM_Sessions/element.js
+++ DOM_Sessions/element.js
@@ -2,6 +2,57 @@ const grandParent = document.querySelector(".grandParent") ;
 const Parent = document.querySelector(".parent") ;
 const Child = document.querySelector(".child") ;
 
-console.log(grandParent) ;
-console.log(parent) ;
-console.log(Child) ;
+// console.log(grandParent) ;
+// console.log(parent) ;
+// console.log(Child) ;
+
+// In Javascript , everything is an event like hovering , clicking etc
+
+
+// Event Bubbling: whwnever we click on child it will print 'child, parent & grandParent' and when we click on Parent it will print 'parent & grandParent'  and again and when we click on grandParent it will print ' grandParent' .clicking check last one is Event or not
+
+grandParent.addEventListener("click" , () => {
+    console.log("grandParent");                                      
+}) ;
+
+
+
+Child.addEventListener("click" , () => {
+    console.log("Child");                                       
+}) ;
+
+
+
+document.querySelector("body").addEventListener("click" , () => {
+    console.log("body") ;
+}) ;
+
+document.querySelector("html").addEventListener("click" , () => {
+    console.log("html") ;
+}) ;
+
+document.addEventListener("click" , () => {
+    console.log("document");                                       
+}) ;
+
+// output:- body
+//         html
+//          document
+// because when we click on body it will check that html is an event or not , if it is an Event then print ,if it is not an Event then don't print . similarly , for document , it will check
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
