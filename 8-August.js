45308129d05448e02cdbf979ecf97d32abf9f4ad
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-09 11:22:06 +0530

    Starting Closures

--- 8-August.js
+++ 8-August.js
@@ -1,4 +1,102 @@
 // Closures
+ 
+
+// var has a functional scope . so, Outside " console.log(name)"  shows Reference error .(name is not defined) 
+function demo() {
+    var name = "Prakash" ;
+    console.log(name) ;
+}
+demo() ;
+console.log(name) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var count = 1;
 
