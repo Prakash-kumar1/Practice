5eec0f391e7bc39ff1f5d500247927bfb7cc4a80
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-10 20:34:13 +0530

    Using Promises for Above example

--- 10-August.js
+++ 10-August.js
@@ -1,29 +1,81 @@
 // // Using CallBack for this example
 
-// entering into the Hotel ,
-// order items ,
-// preparation of food in kitchen,
-// server  delivering food to the table,
-// start eating food ,
-// paying the bill ,
-// leaving the hotel . 
+// // entering into the Hotel ,
+// // order items ,
+// // preparation of food in kitchen,
+// // server  delivering food to the table,
+// // start eating food ,
+// // paying the bill ,
+// // leaving the hotel . 
 
 
-function preparationofFood(callbackOfeatingFood){
-    setTimeout(() => {
-        console.log("Prepareing the PavBhaji... ")
-        console.log("server  delivering food to the table ")
-        callbackOfeatingFood() ;
-    }, 3000);
-}
+// function preparationofFood(callbackOfeatingFood){
+//     setTimeout(() => {
+//         console.log("Prepareing the PavBhaji... ")
+//         console.log("server  delivering food to the table ")
+//         callbackOfeatingFood(payingBillandTips) ;
+//     }, 3000);
+// }
 
-function eatingFood() {
+// function payingBillandTips() {
+//         console.log("paying the bill")
+//         console.log("leaving the hotel ")
+// }
+
+// function eatingFood(callbackOfPayment) {
+//     setTimeout(() => {
+//         console.log("eating the PavBhaji... ")
+//         callbackOfPayment() ;
+//     }, 2000);
+// }
+
+// function dinnerScenario() {
+//     console.log("entering into the Hotel")
+//     console.log("Calling  the server and order PavBhaji")
+//     console.log("server informing the chef to prepare food")
+// preparationofFood(eatingFood) ;    
+// }
+// dinnerScenario() ;
+
+
+// // Using Promises for Above example
+
+
+function preparationofFood(){
+    return new Promise((resolve , reject) => {
+        let vegetablesPresent =  true ;
+        if(vegetablesPresent){
+            setTimeout(() => {
+                console.log("Prepareing the PavBhaji... ")
+                console.log("server  delivering food to the table ")
+                resolve() ;
+            }, 3000);
+        }else{
+            reject("No vegetable is Present") ;
+        }
+     })
+    }
+   
+    function eatingFood() {
+        return new Promise((resolve , reject) => {
     setTimeout(() => {
         console.log("eating the PavBhaji... ")
-        console.log("paying the bill")
-        console.log("leaving the hotel ")
+        resolve() ;
     }, 2000);
+});
 }
+
+function payment() {
+    return new Promise((resolve , reject) => {
+        console.log("paying the bill") ;
+        console.log("leaving the hotel ") ;
+        resolve() ;
+    }) ;
+}
+
+
+
+
 
 function dinnerScenario() {
     console.log("entering into the Hotel")
@@ -31,4 +83,5 @@ function dinnerScenario() {
     console.log("server informing the chef to prepare food")
 preparationofFood(eatingFood) ;    
 }
-dinnerScenario() ;
+
+dinnerScenario() 
