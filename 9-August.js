4d0b53c9040c72872c4db9cb92be12a612a669c4
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-10 12:03:22 +0530

    Update 9-August.js

--- 9-August.js
+++ 9-August.js
@@ -2,9 +2,17 @@
 
 // Asynchronous  Programming
 
+
+function getUser(id){
+    setTimeout(() => {
+        console.log("reading data from the database....") ;
+        return{id: id , githubUserName: "Prakash-kumar"} ;
+    }, 2000) ;
+}
+
 console.log('before') ;
-setTimeout(() => {
-    console.log("reading data from the database....") ;
-    return{id: 1 , githubUserName: "Prakash-kumar"} ;
-}, 2000) ;
+const user = getUser(1) ;
+console.log(user) ;
+// it give result after 2sec . so, my JS program don't wait. by this time the value (Output) is " Undefined " .
+// and again it will print After
 console.log('after') ;
