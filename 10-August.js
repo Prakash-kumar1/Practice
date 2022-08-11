6512b14184eb5c4300e3b973922a9741acc03fa9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-11 17:15:03 +0530

    Important concepts of Promises , async & await

--- 10-August.js
+++ 10-August.js
@@ -9,35 +9,35 @@
 // // leaving the hotel . 
 
 
-// function preparationofFood(callbackOfeatingFood){
-//     setTimeout(() => {
-//         console.log("Prepareing the PavBhaji... ") ;
-//         console.log("server  delivering food to the table") ;
-//         callbackOfeatingFood(payingBillandTips) ;                  // eatingFood
-//     }, 3000);
-// }
+function preparationofFood(callbackOfeatingFood){
+    setTimeout(() => {
+        console.log("Prepareing the PavBhaji... ") ;
+        console.log("server  delivering food to the table") ;
+        callbackOfeatingFood(payingBillandTips) ;                  // eatingFood
+    }, 3000);
+}
 
-// function payingBillandTips() {
-//         console.log("paying the bill") ;
-//         console.log("leaving the hotel ") ;
-// }
+function payingBillandTips() {
+        console.log("paying the bill") ;
+        console.log("leaving the hotel ") ;
+}
 
-// function eatingFood(callbackOfPayment) {
-//     setTimeout(() => {
-//         console.log("eating the PavBhaji... ") ;
-//         callbackOfPayment() ;                                      // payingBillandTips
-//     }, 2000);
-// }
+function eatingFood(callbackOfPayment) {
+    setTimeout(() => {
+        console.log("eating the PavBhaji... ") ;
+        callbackOfPayment() ;                                      // payingBillandTips
+    }, 2000);
+}
 
 // //main code
 
-// function dinnerScenario() {
-//     console.log("entering into the Hotel") ;
-//     console.log("Calling  the server and order PavBhaji") ;
-//     console.log("server informing the chef to prepare food") ;
-// preparationofFood(eatingFood) ;    
-// }
-// dinnerScenario() ;
+function dinnerScenario() {
+    console.log("entering into the Hotel") ;
+    console.log("Calling  the server and order PavBhaji") ;
+    console.log("server informing the chef to prepare food") ;
+preparationofFood(eatingFood) ;    
+}
+dinnerScenario() ;
 
 
 // // Using Promises for Above example
@@ -82,25 +82,25 @@ function payment() {
 }
 
 // // main code
-// function dinnerScenario() {
-//     console.log("entering into the Hotel") ;
-//     console.log("Calling  the server and order PavBhaji") ;
-//     console.log("server informing the chef to prepare food") ;
-// preparationofFood()
-//           .then(() =>{
-//            console.log("food prepared succesfully & delievered") ;
-//             eatingFood().then(() => {
-//                 console.log(" succesfully ate the food");
-//                 payment()
-//                 .then(()=> console.log("process over go to bed and sleep"))
-//                 .catch((error) => console.log("error " , error)) ;    
-//             } )
-//             .catch((error) => console.log(error.message)) ; 
-// })
-//           .catch((error) => console.log("error block-->>" , error)) ;        
-// }
+function dinnerScenario() {
+    console.log("entering into the Hotel") ;
+    console.log("Calling  the server and order PavBhaji") ;
+    console.log("server informing the chef to prepare food") ;
+preparationofFood()
+          .then(() =>{
+           console.log("food prepared succesfully & delievered") ;
+            eatingFood().then(() => {
+                console.log(" succesfully ate the food");
+                payment()
+                .then(()=> console.log("process over go to bed and sleep"))
+                .catch((error) => console.log("error " , error)) ;    
+            } )
+            .catch((error) => console.log(error.message)) ; 
+})
+          .catch((error) => console.log("error block-->>" , error)) ;        
+}
 
-// dinnerScenario() 
+dinnerScenario() 
 
 // // OR  we can write main code this way
 
@@ -130,57 +130,56 @@ function payment() {
 // dinnerScenario() 
 
 
-//   async function dinnerScenario() {
-//     console.log("entering into the Hotel")
-//     console.log("Calling  the server and order PavBhaji")
-//     console.log("server informing the chef to prepare food");
-//     try{
-//         let foodMsg = await preparationofFood()
-//         console.log(foodMsg)
+  async function dinnerScenario() {
+    console.log("entering into the Hotel")
+    console.log("Calling  the server and order PavBhaji")
+    console.log("server informing the chef to prepare food");
+    try{
+        let foodMsg = await preparationofFood()
+        console.log(foodMsg) ;
         
-//         let ateMsg = await eatingFood()
-//         console.log(ateMsg)
+        let ateMsg = await eatingFood()
+        console.log(ateMsg) ;
         
-//         let paymentMsg = await payment()
-//         console.log(paymentMsg)
-//     } catch (err){
-//         console.log("error block-->>" , err)
-//     }
-
-// }
+        let paymentMsg = await payment()
+        console.log(paymentMsg) ;
+    } catch (err){
+        console.log("error block-->>" , err) ;
+    }
+}
 
 
 // // Interview  Question : -->>  How  to  Optimized the Process ??
 // // parellel  OR  concurrent  execution :
 
-// function fn1() {
-//     return new Promise((res , rej) => {
-//         setTimeout(() => {
-//             console.log("process 1")
-//             res("1") ;
-//         }, 5000);
-//     })
-// }
+function fn1() {
+    return new Promise((res , rej) => {
+        setTimeout(() => {
+            console.log("process 1")
+            res("1") ;
+        }, 5000);
+    })
+}
 
-// function fn2() {
-//     return new Promise((res , rej) => {
-//         setTimeout(() => {
-//             console.log("process 2")
-//             res("2") ;
-//         }, 5000);
-//     }) ;
-// }
+function fn2() {
+    return new Promise((res , rej) => {
+        setTimeout(() => {
+            console.log("process 2")
+            res("2") ;
+        }, 5000);
+    }) ;
+}
 
-// // fn1()
-// //    .then(() => fn2())
-// //    .then(() => console.log("Process over")) ;
-// // //    it takes 10 seconds to print . so, we used Promises
+// fn1()
+//    .then(() => fn2())
+//    .then(() => console.log("Process over")) ;
+// //    it takes 10 seconds to print . so, we used Promises
 
 
-// Promise.all([fn1() , fn2()]).then((result) =>
-// console.log("process over" , result)
-// ) ;
-// // it takes 5 seconds to print .
+Promise.all([fn1() , fn2()]).then((result) =>
+console.log("process over" , result)
+) ;
+// it takes 5 seconds to print .
 
 
 // if i reject any of the function then it will automatically reject whole function . we can solve problem this way
@@ -203,12 +202,12 @@ function fn2() {
     }) ;
 }
 
-// Promise.all([fn1() , fn2()])
-// .then((result) => console.log("process over" , result))
+Promise.all([fn1() , fn2()])
+.then((result) => console.log("process over" , result))
 
-// .catch((err) => console.log("error block-->>" , err))  
+.catch((err) => console.log("error block-->>" , err))  
 
-// .finally(() => console.log("closing connection")) ;
+.finally(() => console.log("closing connection")) ;
 
 
 // Whoever completeing first , i want to print that data
