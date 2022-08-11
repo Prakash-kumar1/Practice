5f520239b4b3231645ead7220f3f76941f84da04
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-11 20:17:55 +0530

    Update 11-August.js

--- 11-August.js
+++ 11-August.js
@@ -1,3 +1,15 @@
 // Promises performs Asynchronous operation
 
 console.log(typeof new Promise((res , rej) => {} ))                        // Output ==>> object
+
+let p1 = Promise.resolve("p1 data") ;
+let p2 = Promise.resolve("p2 Print") ;
+console.log(p1 , p2) ;
+
+
+
+
+
+
+
+
