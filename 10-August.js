159d5cfe5e68b3e1a762c08d1a070c101fb27477
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-11 13:39:31 +0530

    Revising  Promises

--- 10-August.js
+++ 10-August.js
@@ -9,33 +9,33 @@
 // // leaving the hotel . 
 
 
-// function preparationofFood(callbackOfeatingFood){
-//     setTimeout(() => {
-//         console.log("Prepareing the PavBhaji... ")
-//         console.log("server  delivering food to the table ")
-//         callbackOfeatingFood(payingBillandTips) ;
-//     }, 3000);
-// }
+function preparationofFood(callbackOfeatingFood){
+    setTimeout(() => {
+        console.log("Prepareing the PavBhaji... ")
+        console.log("server  delivering food to the table ")
+        callbackOfeatingFood(payingBillandTips) ;
+    }, 3000);
+}
 
-// function payingBillandTips() {
-//         console.log("paying the bill")
-//         console.log("leaving the hotel ")
-// }
+function payingBillandTips() {
+        console.log("paying the bill")
+        console.log("leaving the hotel ")
+}
 
-// function eatingFood(callbackOfPayment) {
-//     setTimeout(() => {
-//         console.log("eating the PavBhaji... ")
-//         callbackOfPayment() ;
-//     }, 2000);
-// }
+function eatingFood(callbackOfPayment) {
+    setTimeout(() => {
+        console.log("eating the PavBhaji... ")
+        callbackOfPayment() ;
+    }, 2000);
+}
 
-// function dinnerScenario() {
-//     console.log("entering into the Hotel")
-//     console.log("Calling  the server and order PavBhaji")
-//     console.log("server informing the chef to prepare food")
-// preparationofFood(eatingFood) ;    
-// }
-// dinnerScenario() ;
+function dinnerScenario() {
+    console.log("entering into the Hotel")
+    console.log("Calling  the server and order PavBhaji")
+    console.log("server informing the chef to prepare food")
+preparationofFood(eatingFood) ;    
+}
+dinnerScenario() ;
 
 
 // // Using Promises for Above example
@@ -123,24 +123,24 @@
 // dinnerScenario() 
 
 
-  async function dinnerScenario() {
-    console.log("entering into the Hotel")
-    console.log("Calling  the server and order PavBhaji")
-    console.log("server informing the chef to prepare food");
-    try{
-        let foodMsg = await preparationofFood()
-        console.log(foodMsg)
+//   async function dinnerScenario() {
+//     console.log("entering into the Hotel")
+//     console.log("Calling  the server and order PavBhaji")
+//     console.log("server informing the chef to prepare food");
+//     try{
+//         let foodMsg = await preparationofFood()
+//         console.log(foodMsg)
         
-        let ateMsg = await eatingFood()
-        console.log(ateMsg)
+//         let ateMsg = await eatingFood()
+//         console.log(ateMsg)
         
-        let paymentMsg = await payment()
-        console.log(paymentMsg)
-    } catch (err){
-        console.log("error block-->>" , err)
-    }
+//         let paymentMsg = await payment()
+//         console.log(paymentMsg)
+//     } catch (err){
+//         console.log("error block-->>" , err)
+//     }
 
-}
+// }
 
 
 // parellel  OR  concurrent  execution
