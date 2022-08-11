462e7ebd4b56c8496e5dcce69d09b960d8c6fb0e
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-11 14:36:29 +0530

    Update 10-August.js

--- 10-August.js
+++ 10-August.js
@@ -9,97 +9,97 @@
 // // leaving the hotel . 
 
 
-function preparationofFood(callbackOfeatingFood){
-    setTimeout(() => {
-        console.log("Prepareing the PavBhaji... ") ;
-        console.log("server  delivering food to the table") ;
-        callbackOfeatingFood(payingBillandTips) ;                  // eatingFood
-    }, 3000);
-}
+// function preparationofFood(callbackOfeatingFood){
+//     setTimeout(() => {
+//         console.log("Prepareing the PavBhaji... ") ;
+//         console.log("server  delivering food to the table") ;
+//         callbackOfeatingFood(payingBillandTips) ;                  // eatingFood
+//     }, 3000);
+// }
 
-function payingBillandTips() {
-        console.log("paying the bill") ;
-        console.log("leaving the hotel ") ;
-}
+// function payingBillandTips() {
+//         console.log("paying the bill") ;
+//         console.log("leaving the hotel ") ;
+// }
 
-function eatingFood(callbackOfPayment) {
-    setTimeout(() => {
-        console.log("eating the PavBhaji... ") ;
-        callbackOfPayment() ;                                      // payingBillandTips
-    }, 2000);
-}
+// function eatingFood(callbackOfPayment) {
+//     setTimeout(() => {
+//         console.log("eating the PavBhaji... ") ;
+//         callbackOfPayment() ;                                      // payingBillandTips
+//     }, 2000);
+// }
 
-//main code
- 
-function dinnerScenario() {
-    console.log("entering into the Hotel") ;
-    console.log("Calling  the server and order PavBhaji") ;
-    console.log("server informing the chef to prepare food") ;
-preparationofFood(eatingFood) ;    
-}
-dinnerScenario() ;
+// //main code
+
+// function dinnerScenario() {
+//     console.log("entering into the Hotel") ;
+//     console.log("Calling  the server and order PavBhaji") ;
+//     console.log("server informing the chef to prepare food") ;
+// preparationofFood(eatingFood) ;    
+// }
+// dinnerScenario() ;
 
 
 // // Using Promises for Above example
 
 
-// function preparationofFood(){
-//     return new Promise((resolve , reject) => {
-//         let vegetablesPresent =  true ;
-//         if(vegetablesPresent){
-//             setTimeout(() => {
-//                 console.log("Prepareing the PavBhaji... ")
-//                 console.log("server  delivering food to the table ")
-//                 resolve() ;
-//             }, 3000);
-//         }else{
-//             reject("No vegetable is Present") ;
-//         }
-//      })
-//     }
+function preparationofFood(){
+    return new Promise((resolve , reject) => {
+        let vegetablesPresent =  true ;
+        if(vegetablesPresent){
+            setTimeout(() => {
+                console.log("Prepareing the PavBhaji... ")
+                console.log("server  delivering food to the table ")
+                resolve() ;
+            }, 3000);
+        }else{
+            reject("No vegetable is Present") ;
+        }
+     })
+    }
    
-//     function eatingFood() {
-//         return new Promise((resolve , reject) => {
-//     setTimeout(() => {
-//         console.log("eating the PavBhaji... ")
-//         resolve() ;
-//     }, 2000);
-// });
-// }
+    function eatingFood() {
+        return new Promise((resolve , reject) => {
+    setTimeout(() => {
+        console.log("eating the PavBhaji... ")
+        resolve() ;
+    }, 2000);
+});
+}
 
-// function payment() {
-//     return new Promise((resolve , reject) => {
-//         let pursePresent = true ;
-//         if(pursePresent){
-//             console.log("paying the bill") ;
-//             console.log("leaving the hotel ") ;
-//             resolve() ;
-//         }else{
-//             reject("go and washing the Plates ")
-//         }
+function payment() {
+    return new Promise((resolve , reject) => {
+        let pursePresent = true ;
+        if(pursePresent){
+            console.log("paying the bill") ;
+            console.log("leaving the hotel ") ;
+            resolve() ;
+        }else{
+            reject("go and washing the Plates ")
+        }
         
-//     }) ;
-// }
+    }) ;
+}
 
-// function dinnerScenario() {
-//     console.log("entering into the Hotel")
-//     console.log("Calling  the server and order PavBhaji")
-//     console.log("server informing the chef to prepare food")
-// preparationofFood()
-//           .then(() =>{
-//            console.log("food prepared succesfully & delievered") ;
-//             eatingFood().then(() => {
-//                 console.log(" succesfully ate the food");
-//                 payment()
-//                 .then(()=> console.log("process over go to bed and sleep"))
-//                 .catch((error) => console.log("error " , error)) ;    
-//             } )
-//             .catch((error) => console.log(error.message)) ; 
-// })
-//           .catch((error) => console.log("error block-->>" , error)) ;        
-// }
+function dinnerScenario() {
+    console.log("entering into the Hotel")
+    console.log("Calling  the server and order PavBhaji")
+    console.log("server informing the chef to prepare food")
+preparationofFood()
+          .then(() =>{
+           console.log("food prepared succesfully & delievered") ;
+            eatingFood().then(() => {
+                console.log(" succesfully ate the food");
+                payment()
+                .then(()=> console.log("process over go to bed and sleep"))
+                .catch((error) => console.log("error " , error)) ;    
+            } )
+            .catch((error) => console.log(error.message)) ; 
+})
+          .catch((error) => console.log("error block-->>" , error)) ;        
+}
 
-// dinnerScenario() 
+dinnerScenario() 
 
 
 
