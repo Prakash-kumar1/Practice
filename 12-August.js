c21d814dca8b461c95c2038e0cbd1273a64ad8a9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-15 11:46:57 +0530

    Update 12-August.js

--- 12-August.js
+++ 12-August.js
@@ -4,31 +4,33 @@
 // objects -->  instance of class
 
 class Human {
-    constructor(name , age , color) {
+    constructor(name , age , gender) {
         this.name = name ;
         this.age = age ;
-        this.color = color ;
+        this.gender = gender ;
     }
 
 eat() {
     // console.log("eating") ;
-    return eating ;
+    return 'eating' ;
 }
 
 think() {
     // console.log("thinking") ;
-    return thinking ;
+    return 'thinking' ;
 }
 
 sleep() {
-    // console.log("sleeping") ;
-    return sleeping ;
+    console.log("sleeping") ;
+    // return 'sleeping' ;
 }
 }
 
 let prakash = new Human ("Prakash" , 22 , "Alpha Male") ;
-console.log(prakash , prakash.think()) ;
-let rani =  new Human ("Rinki" , 20 , "Female") ;
+// console.log(prakash) ;                                        // Output :-    Human { name: 'Prakash', age: 22, gender: 'Alpha Male' }
+
+console.log(prakash , prakash.think()) ;                         // Output :-    Human { name: 'Prakash', age: 22, gender: 'Alpha Male' } thinking
+// let rani =  new Human ("Rinki" , 20 , "Female") ;
 
 
 
