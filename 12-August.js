407bd30d01d3bbc7ee166af5d6ec6a483d2583aa
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-15 19:39:34 +0530

    Inheritance ,  Polymorphism  and  Classes  Concepts

--- 12-August.js
+++ 12-August.js
@@ -5,59 +5,59 @@
 
 // // Example no. ==>> 1
 
-// class Human {
-//     constructor(name , age , gender) {
-//         this.name = name ;
-//         this.age = age ;
-//         this.gender = gender ;
-//     }
+class Human {
+    constructor(name , age , gender) {
+        this.name = name ;
+        this.age = age ;
+        this.gender = gender ;
+    }
 
-// eat() {
-//     // console.log("eating") ;
-//     return 'eating' ;
-// }
+eat() {
+    // console.log("eating") ;
+    return 'eating' ;
+}
 
-// think() {
-//     // console.log("thinking") ;
-//     return 'thinking' ;
-// }
+think() {
+    // console.log("thinking") ;
+    return 'thinking' ;
+}
 
-// sleep() {
-//     console.log("sleeping") ;
-//     // return 'sleeping' ;
-// }
-// }
+sleep() {
+    console.log("sleeping") ;
+    // return 'sleeping' ;
+}
+}
 
-// let prakash = new Human ("Prakash" , 22 , "Alpha Male") ;
-// console.log(prakash , prakash.think()) ;                         // Output :-    Human { name: 'Prakash', age: 22, gender: 'Alpha Male' } thinking
+let prakash = new Human ("Prakash" , 22 , "Alpha Male") ;
+console.log(prakash , prakash.think()) ;                         // Output :-    Human { name: 'Prakash', age: 22, gender: 'Alpha Male' } thinking
 
 
-// let rani =  new Human ("Rinki" , 20 , "Female") ;
-// console.log(rani , rani.eat()) ;                                  // Output :-      Human { name: 'Rinki', age: 20, gender: 'Female' } eating
+let rani =  new Human ("Rinki" , 20 , "Female") ;
+console.log(rani , rani.eat()) ;                                  // Output :-      Human { name: 'Rinki', age: 20, gender: 'Female' } eating
 
 
 // // Example no. ==>> 2
 
 
-// class Car {
-//     constructor(model , color , brand , type) {
-//         this.model = model ;
-//         this.color = color ;
-//         this.brand = brand ;
-//         this.type = type ;
-//     }
+class Car {
+    constructor(model , color , brand , type) {
+        this.model = model ;
+        this.color = color ;
+        this.brand = brand ;
+        this.type = type ;
+    }
 
-//     run() {
-//         return ("running") ;
-//     }
+    run() {
+        return ("running") ;
+    }
 
-//     playSongs() {
-//         console.log("Hollywood Songs")
-//     }
-// }
+    playSongs() {
+        console.log("Hollywood Songs")
+    }
+}
 
-// let hondaCity = new Car("2022" , "red" , "Maruti" , "Sedan") ;
-// console.log(hondaCity , hondaCity.run()) ;
+let hondaCity = new Car("2022" , "red" , "Maruti" , "Sedan") ;
+console.log(hondaCity , hondaCity.run()) ;
 
 // // Output:- Car { model: '2022', color: 'red', brand: 'Maruti', type: 'Sedan' } running
 
@@ -65,40 +65,40 @@
 // // Inheritance :
 
 
-// class Parent {
-//     car() {
-//         console.log("maruti espresso") ;
-//     }
-// }
+class Parent {
+    car() {
+        console.log("maruti espresso") ;
+    }
+}
 
-// class Child {}
+class Child {}
 
-// let vasant =  new Parent() ;
-// vasant.car() ;                                                                // Output :-   maruti espresso
+let vasant =  new Parent() ;
+vasant.car() ;                                                                // Output :-   maruti espresso
 
-// But grand child don't access the Properties
+But grand child don't access the Properties
 
-// let aditi = new Child() ;
-// console.log(aditi) ;                                                        // Output :-   Child{}
-// aditi.car() ;                                                              // Output :- TypeError: aditi.car is not a function 
+let aditi = new Child() ;
+console.log(aditi) ;                                                        // Output :-   Child{}
+aditi.car() ;                                                              // Output :- TypeError: aditi.car is not a function 
 
 
 
-// // For removing This error we use  " extends "
+// For removing This error we use  " extends "
 
-// class Parent {
-//     car() {
-//         console.log("maruti espresso") ;
-//     }
-// }
+class Parent {
+    car() {
+        console.log("maruti espresso") ;
+    }
+}
 
-// class Child extends Parent{}
+class Child extends Parent{}
 
-// let vasanth =  new Parent() ;
-// vasanth.car() ;                                                              // Output :-   maruti espresso
+let vasanth =  new Parent() ;
+vasanth.car() ;                                                              // Output :-   maruti espresso
 
-// let aditya = new Child() ;
-// aditya.car() ;                                                             // Output :-   maruti espresso
+let aditya = new Child() ;
+aditya.car() ;                                                             // Output :-   maruti espresso
 
 
 // // // If a parent has a property then child has used it , But  if a child has a extra property  then parent  don't  access  it .
