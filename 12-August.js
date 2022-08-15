4fe0fba11f0497970e7723ac3d1b1eb656567fbd
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-15 11:57:24 +0530

    Update 12-August.js

--- 12-August.js
+++ 12-August.js
@@ -1,41 +1,65 @@
-// How  Internet  works , Server , OSI Model , Computer Networks read it on your own ??
+// // How  Internet  works , Server , OSI Model , Computer Networks read it on your own ??
 
-// class -->  it is a blue-print
-// objects -->  instance of class  OR   Real time entity
+// // class -->  it is a blue-print
+// // objects -->  instance of class  OR   Real time entity
+
+// // Example no. ==>> 1
+
+// class Human {
+//     constructor(name , age , gender) {
+//         this.name = name ;
+//         this.age = age ;
+//         this.gender = gender ;
+//     }
+
+// eat() {
+//     // console.log("eating") ;
+//     return 'eating' ;
+// }
+
+// think() {
+//     // console.log("thinking") ;
+//     return 'thinking' ;
+// }
+
+// sleep() {
+//     console.log("sleeping") ;
+//     // return 'sleeping' ;
+// }
+// }
+
+// let prakash = new Human ("Prakash" , 22 , "Alpha Male") ;
+// console.log(prakash , prakash.think()) ;                         // Output :-    Human { name: 'Prakash', age: 22, gender: 'Alpha Male' } thinking
 
 
-class Human {
-    constructor(name , age , gender) {
-        this.name = name ;
-        this.age = age ;
-        this.gender = gender ;
+// let rani =  new Human ("Rinki" , 20 , "Female") ;
+// console.log(rani , rani.eat()) ;                                  // Output :-      Human { name: 'Rinki', age: 20, gender: 'Female' } eating
+
+
+// // Example no. ==>> 2
+
+
+class Car {
+    constructor(model , color , brand , type) {
+        this.model = model ;
+        this.color = color ;
+        this.brand = brand ;
+        this.type = type ;
     }
 
-eat() {
-    // console.log("eating") ;
-    return 'eating' ;
+    run() {
+        return ("running") ;
+    }
+
+    playSongs() {
+        console.log("Hollywood Songs")
+    }
 }
 
-think() {
-    // console.log("thinking") ;
-    return 'thinking' ;
-}
+let hondaCity = new Car("2022" , "red" , "Maruti" , "Sedan") ;
+console.log(hondaCity , hondaCity.run()) ;
 
-sleep() {
-    console.log("sleeping") ;
-    // return 'sleeping' ;
-}
-}
-
-let prakash = new Human ("Prakash" , 22 , "Alpha Male") ;
-console.log(prakash , prakash.think()) ;                         // Output :-    Human { name: 'Prakash', age: 22, gender: 'Alpha Male' } thinking
-
-
-let rani =  new Human ("Rinki" , 20 , "Female") ;
-console.log(rani , rani.eat()) ;                                  // Output :-      Human { name: 'Rinki', age: 20, gender: 'Female' } eating
-
-
-
+// Output:- Car { model: '2022', color: 'red', brand: 'Maruti', type: 'Sedan' } running
 
 
 // // Inheritance :
