bfbddbdb873e6f381abf8b8748ced42868ea6843
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-15 11:38:42 +0530

    revising

--- 12-August.js
+++ 12-August.js
@@ -1,8 +1,7 @@
-// Question no.-1 :  How  Internet  works ??
+// How  Internet  works , Server , OSI Model  read it on your own??
 
-// server :- A Computer without  OS 
-
-// Advantage  of class using  over  Function ??
+// class -->  it is a blue-print
+// objects -->  instance of class
 
 class Human {
     constructor(name , age , color) {
@@ -31,20 +30,22 @@ let prakash = new Human ("Prakash" , 22 , "Alpha Male") ;
 console.log(prakash , prakash.think()) ;
 let rani =  new Human ("Rinki" , 20 , "Female") ;
 
-// Inheritance :
 
-class Parent {
-    car() {
-        console.log("maruti espresso") ;
-    }
-}
 
-class Child extends Parent{} 
+// // Inheritance :
 
-let vaanth = new Parent() 
-vaanth.car () ;
+// class Parent {
+//     car() {
+//         console.log("maruti espresso") ;
+//     }
+// }
 
-let aditya = new Child() 
-aditya.car() ;
+// class Child extends Parent{} 
 
-// Polymorphism :
+// let vaanth = new Parent() 
+// vaanth.car () ;
+
+// let aditya = new Child() 
+// aditya.car() ;
+
+// // Polymorphism :
