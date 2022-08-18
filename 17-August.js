f927c816a9492f3f592d47a33ffba4abb5f4b6e9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-18 19:41:27 +0530

    Update 17-August.js

--- 17-August.js
+++ 17-August.js
@@ -1,26 +1,37 @@
-// 17 August session
+// // 17 August session
 
-Promise.resolve('Success!')
-  .then(() => {
-    throw Error('Oh noes!')
-  })
-  .catch(error => {
-    return 'actually, that worked'
-  })
-  .then(data => {
-    throw Error('The fails!')
-  })
-  .catch(error => console.log(error.message))
+// Promise.resolve('Success!')
+//   .then(() => {
+//     throw Error('Oh noes!')
+//   })
+//   .catch(error => {
+//     return 'actually, that worked'
+//   })
+//   .then(data => {
+//     throw Error('The fails!')
+//   })
+//   .catch(error => console.log(error.message))
 
 
 
-  new Promise((resolve, reject) => {
-    resolve('Welcome!!')
-  })
-  .then(() => {
-    throw Error('Do not go anywhere')
-  })
-  .catch(error => {
-    return "Come to the village"
-  })
-  .catch(error => console.log(error.message))
+//   new Promise((resolve, reject) => {
+//     resolve('Welcome!!')
+//   })
+//   .then(() => {
+//     throw Error('Do not go anywhere')
+//   })
+//   .catch(error => {
+//     return "Come to the village"
+//   })
+//   .catch(error => console.log(error.message))
+
+// 18 August session
+ 
+var sum = 0.1+0.2
+// console.log(sum)
+
+if(sum == 0.3){
+  console.log("true")
+}else{
+  console.log("false") ;
+}
