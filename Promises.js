79379cbffb63f3575d6aa634af9c0835c0d3b068
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-21 21:24:18 +0530

    Promises  ,,  Async and Await ,, Callback hell

--- Promises.js
+++ Promises.js
@@ -137,23 +137,152 @@
 // ) ;
 
 
-// // CATCH method :-     // syntax:-  catch(callback)
+// // // CATCH method :-     // syntax:-  catch(callback)
 
-// // Where the callback is a function called when the Promise is Rejected. This function has 
-// // one argument error -> The Rejection Error
+// // // Where the callback is a function called when the Promise is Rejected. This function has 
+// // // one argument error -> The Rejection Error
 
-const promiseName = new Promise((res,rej)=>{     
-    let req = 67                       
-    if(req === 34){                         
-    res("Request Success");                       
-    }else{                          
-    rej("Request rejected") ;                         
-    }                           
- });
-promiseName
-           .then((value) => console.log(value))
-           .catch((error) => console.log(error)) ;
+// const promiseName = new Promise((res,rej)=>{     
+//     let req = 67                       
+//     if(req === 34){                         
+//     res("Request Success");                       
+//     }else{                          
+//     rej("Request rejected") ;                         
+//     }                           
+//  });
+// promiseName
+//            .then((value) => console.log(value))
+//            .catch((error) => console.log(error)) ;
 
                         
 
-// FINALLY method :
+// // // FINALLY method : it runs in both case (res or rej)
+
+// const prakash = new Promise((res,rej)=>{     
+//     let req = 67                       
+//     if(req === 34){                         
+//     res("Request Success");                       
+//     }else{                          
+//     rej("Request rejected") ;                         
+//     }                           
+//  });
+// prakash
+//            .then((value) => console.log(value))
+//            .catch((error) => console.log(error)) 
+//            .finally(()=>console.log("Cleaned")) ;
+
+
+//  //    Example=>1  for using Catch , we give intentionally  Error
+
+// const promiseObj = new Promise((res,rej)=>{ 
+//   let num = 10 ;
+//   res(num);
+// }).then(
+//     (value) => {console.log(value) ;
+//     return value + 10 ;
+//     }
+// ).then(
+//     (value)=>{
+//         console.log(value) ;
+//         return value ;
+//     }
+// ).then(      // intentionally putting value = 0 for checking Error
+//     (value = 0) => {
+//                throw new Error("Error done intentionally / Deliberately") 
+// }
+// ).catch((error)=> console.log(error)) ;
+
+// // output ==>>   Error: Error done intentionally / Deliberately
+
+
+// //  //    Example=>2
+
+// console.log("starting Callback Hell") ;
+
+// function getName(name , callback){
+//     setTimeout(() => {
+//         console.log("Name Inside Set-Timeout") ;
+//         callback (name) ;
+//     }, 3000);
+// }
+// function getHobbies(name , callback){
+//     setTimeout(() => {
+//         console.log(" Inside Hobbies Set-Timeout") ;
+//         callback (['cricket' , 'Reading' , 'Dancing']) ;
+//     }, 1000);
+// }
+
+// const nm = getName("Shivansh Rawat" , (nm)=>{ getHobbies(nm , hobby =>{console.log(hobby)})
+                                                 
+// }) ;
+
+// console.log("Ending ") ;
+
+// // //  Solving  Above Callback hell Example  by using  Promise
+
+// console.log("solving Callback Hell example with Promise") ;
+
+// function getName(name ){
+//    return new Promise((res,rej)=>{ 
+//     setTimeout(() => {
+//         console.log("Name Inside Set-Timeout") ;
+//         res (name) ;
+//     }, 3000);
+// })
+// }
+
+// function getHobbies(hobby ){
+//     return new Promise((res,rej)=>{ 
+//      setTimeout(() => {
+//          console.log(" Inside Hobbies Set-Timeout") ;
+//          res (['cricket' , 'Reading' , 'Dancing']) ;
+//      }, 3000);
+//  })
+//  }
+ 
+// getName('Govind Verma')
+//                       .then(nm => getHobbies(nm))
+//                       .then(hobby => console.log(hobby))
+//                       .catch((error)=>console.log(error)) ;
+
+// console.log("Ending Promise") ;
+
+// //  Output of Callback Hell and Promise are same =>   solving Callback Hell example with Promise
+// //                                                    Name Inside Set-Timeout
+// //                                                    Inside Hobbies Set-Timeout
+// //                                                    [ 'cricket', 'Reading', 'Dancing' ]   
+// //                                                     Ending Promise
+
+
+// // //  Solving  Above Promise Example  by using  Async and Await  && for error  try and catch
+
+console.log("solving Promise example with Async and Await") ;
+
+function getName(name ){
+   return new Promise((res,rej)=>{ 
+    setTimeout(() => {
+        console.log("Name Inside Set-Timeout") ;
+        res (name) ;
+    }, 3000);
+})
+}
+
+function getHobbies(hobby ){
+    return new Promise((res,rej)=>{ 
+     setTimeout(() => {
+         console.log(" Inside Hobbies Set-Timeout") ;
+         res (['cricket' , 'Reading' , 'Dancing']) ;
+     }, 3000);
+ })
+ }
+
+async function showHobby(){
+    try{
+        const nm = await getName('prakash')
+    const hobby = await getHobbies(nm)
+    console.log(hobby)
+    } catch {
+        console.log("For handling error Async and Await in we use try and catch")
+    }
+    }
+showHobby() ;
