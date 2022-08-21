4e850d7bb0923af4359870dd0e124406b560e775
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-21 19:51:30 +0530

    starting   Protypical  Inheritance

---
+++ 21-August.js/about.js
@@ -0,0 +1,25 @@
+// Protypical  Inheritance :-
+
+let nums = [1,2,3,4,5] ;
+// prototype -->> my methods
+// _proto_ -->> my parent methods
+
+
+let person = {
+    name : "Prakash" ,
+    age : 16 ,
+};
+
+let person2 = {
+    name : "govind" ,
+};
+
+
+person2._proto_  = person
+
+
+
+
+
+
+
