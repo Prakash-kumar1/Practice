8feb67ebb9178cede868eec85a59fba49b736c24
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-22 23:03:13 +0530

    Object Prototypes

--- Prototype.js/about.js
+++ Prototype.js/about.js
@@ -233,18 +233,15 @@ console.log(nums6) ;
 // #### Polyfills for Filter done , Now we done Differnt Operations ####
 
 
-// Making Polyfill  for Reduce  (21-August Sunday - session):-
+// // Making Polyfill  for Reduce  (21-August Sunday - session):-
 
-// By default  intialValue  is 0 
-Array.prototype.myFilter = function(callbackfn , intialValue){
-    let acc  =  intialValue ;
-
-
-
-}
+// // By default  intialValue  is 0 
+// Array.prototype.myFilter = function(callbackfn , intialValue){
+//     let acc  =  intialValue ;
+// }
 
 
-// #### Polyfills for Reduce done , Now we done Differnt Operations ####
+// // #### Polyfills for Reduce done , Now we done Differnt Operations ####
 
 
 
