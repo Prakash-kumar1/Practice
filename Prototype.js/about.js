481af6d5900e1478e5b9c11f4eabbc6b583277b3
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-23 18:29:16 +0530

    Update about.js

--- Prototype.js/about.js
+++ Prototype.js/about.js
@@ -235,10 +235,27 @@
 // // Making Polyfill  for Reduce  (21-August Sunday - session):-
 
 // // By default  intialValue  is 0 
-// Array.prototype.myFilter = function(callbackfn , intialValue){
-//     let acc  =  intialValue ;
-// }
 
+// Array.prototype.myReduce = function (callbackFn, initialValue) {
+//     let acc = initialValue;
+//     for (let i = 0; i < this.length; i++) {
+//       if (acc !== undefined) {
+//         acc = callbackFn(acc, this[i]);
+//       } else {
+//         acc = this[i];
+//       }
+//     }
+//     return acc;
+//   };
+  
+// // // #### Polyfills for Reduce done , Now we done Differnt Operations ####
+
+//   let reducedNum = [1, 2, 3, 4].myReduce((acc, current) => {
+//     // console.log(acc, current);
+//     return acc + current;
+//   });
+  
+//   console.log(reducedNum);                //10
 
 // // #### Polyfills for Reduce done , Now we done Differnt Operations ####
 
