f3c12f8e6f736cbed0c9d486db031143f45a7f97
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-23 15:18:05 +0530

    Inheritance

--- 12-August.js
+++ 12-August.js
@@ -5,59 +5,59 @@
 
 // // Example no. ==>> 1
 
-class Human {
-    constructor(name , age , gender) {
-        this.name = name ;
-        this.age = age ;
-        this.gender = gender ;
-    }
+// class Human {
+//     constructor(name , age , gender) {
+//         this.name = name ;
+//         this.age = age ;
+//         this.gender = gender ;
+//     }
 
-eat() {
-    // console.log("eating") ;
-    return 'eating' ;
-}
+// eat() {
+//     // console.log("eating") ;
+//     return 'eating' ;
+// }
 
-think() {
-    // console.log("thinking") ;
-    return 'thinking' ;
-}
+// think() {
+//     // console.log("thinking") ;
+//     return 'thinking' ;
+// }
 
-sleep() {
-    console.log("sleeping") ;
-    // return 'sleeping' ;
-}
-}
+// sleep() {
+//     console.log("sleeping") ;
+//     // return 'sleeping' ;
+// }
+// }
 
-let prakash = new Human ("Prakash" , 22 , "Alpha Male") ;
-console.log(prakash , prakash.think()) ;                         // Output :-    Human { name: 'Prakash', age: 22, gender: 'Alpha Male' } thinking
+// let prakash = new Human ("Prakash" , 22 , "Alpha Male") ;
+// console.log(prakash , prakash.think()) ;                         // Output :-    Human { name: 'Prakash', age: 22, gender: 'Alpha Male' } thinking
 
-let rani =  new Human ("Rinki" , 20 , "Female") ;
-console.log(rani , rani.eat()) ;                                  // Output :-      Human { name: 'Rinki', age: 20, gender: 'Female' } eating
-
-
-// Example no. ==>> 2
+// let rani =  new Human ("Rinki" , 20 , "Female") ;
+// console.log(rani , rani.eat()) ;                                  // Output :-      Human { name: 'Rinki', age: 20, gender: 'Female' } eating
 
 
-class Car {
-    constructor(model , color , brand , type) {
-        this.model = model ;
-        this.color = color ;
-        this.brand = brand ;
-        this.type = type ;
-    }
+// // Example no. ==>> 2
 
-    run() {
-        return ("running") ;
-    }
 
-    playSongs() {
-        // console.log("Hollywood Songs")
-        return("Hollywood Songs")
-    }
-}
+// class Car {
+//     constructor(model , color , brand , type) {
+//         this.model = model ;
+//         this.color = color ;
+//         this.brand = brand ;
+//         this.type = type ;
+//     }
 
-let hondaCity = new Car("2022" , "red" , "Maruti" , "Sedan") ;
-console.log(hondaCity , hondaCity.run() , hondaCity.playSongs()) ;
+//     run() {
+//         return ("running") ;
+//     }
+
+//     playSongs() {
+//         // console.log("Hollywood Songs")
+//         return("Hollywood Songs")
+//     }
+// }
+
+// let hondaCity = new Car("2022" , "red" , "Maruti" , "Sedan") ;
+// console.log(hondaCity , hondaCity.run() , hondaCity.playSongs()) ;
 
 // // Output:- Car { model: '2022', color: 'red', brand: 'Maruti', type: 'Sedan' } running
 
