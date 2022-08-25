dbe8be6b6ac60feb1260ce3d850549c216bbe05f
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-25 23:02:42 +0530

    Update 24-Aug.js

--- 24-Aug.js
+++ 24-Aug.js
@@ -22,33 +22,65 @@
 
 // // Currying Function
 
-// Example:-1
-function sum(a){
-     return (b) =>{
-      return (c) =>{
-         return (d) =>{
-            return a+b+c+d ;
-         }
-      }  
-     }
-}
-console.log(sum((10,20,30,40)))       //No Output
-// this is the way , you can pass argument in  Currying Function
-console.log(sum(10)(20)(30)(40)) ;
+// // Example:-1
+// function sum(a){
+//      return (b) =>{
+//       return (c) =>{
+//          return (d) =>{
+//             return a+b+c+d ;
+//          }
+//       }  
+//      }
+// }
+// console.log(sum((10,20,30,40)))       //No Output
+// // this is the way , you can pass argument in  Currying Function
+// console.log(sum(10)(20)(30)(40)) ;
 
 
-// Example:-2
-function power(a){
-    return (b) =>{
-     return (c) =>{
-        return (d) =>{
-           return a**b**c**d ;
-        }
-     }  
-    }
+// // Example:-2
+// function power(a){
+//     return (b) =>{
+//      return (c) =>{
+//         return (d) =>{
+//            return a**b**c**d ;
+//         }
+//      }  
+//     }
+// }
+// // this is the way , you can pass argument in  Currying Function
+// console.log(power(2)(2)(1)(0)) ;      //4
+
+// // Example:-3
+// function sub(a){          //main(1) main(3)  main(6) a=10
+//     return (b) =>{       //b=2    b=4    b=" "
+//         if(b) return sub(a+b)     //main(3) main(6)  main(10)
+//         return a ;
+//     }
+// }
+// console.log(sub(1)(2)(3)(4)()) ;
+
+// // Example:-4
+let addition = function(a,b){
+    return a+b ;
 }
-// this is the way , you can pass argument in  Currying Function
-console.log(power(2)(2)(1)(0)) ;      //4
+let addition1 = addition.bind(this,1)
+console.log(addition1(3,5))       //4
+
+
+// // Example:-5
+let addition3 = function(a,b){
+    return a+b ;
+}
+let addition4 = addition3.bind(this,2,7)
+console.log(addition4(3,5))       //9
+
+
+// // Example:-6
+let addition5 = function(a,b,c,d){
+    return a+b+c+d ;
+}
+let addition6 = addition5.bind(this,2,7)
+console.log(addition6(1,2))       //12
 
 
 // Currrying --  Currying is a process in functional programming  in which we can transform a function with multiple arguments into a sequence of nesting function . It returns a new function that excepts the next argument inline .
