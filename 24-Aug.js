2b5fd20b5336556b645401f8f5a8ad306bd31bc0
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-25 23:09:26 +0530

    Currying  Functions  and  Problems

--- 24-Aug.js
+++ 24-Aug.js
@@ -1,65 +1,65 @@
-// // Example of Closure ==>> how to print "aircampus" ?? 
+// Example of Closure ==>> how to print "aircampus" ?? 
 
-// function main(){
-//     var firstName = "Aircampus" ;
-//     function writeResult(){
-//         console.log(firstName)
-//     }
-//     writeResult() ;
-// }
-// main() ;
-
-
-// // Functional Programming are of 3 Types
-// // Pure Function , HOF , Currying Functions
-
-// // Normal Function
-// function add(a,b,c,d){
-//     return a+b+c+d ;
-// }
-// console.log(add(1,2,3,4)) ;     //10
+function main(){
+    var firstName = "Aircampus" ;
+    function writeResult(){
+        console.log(firstName)
+    }
+    writeResult() ;
+}
+main() ;
 
 
-// // Currying Function
+// Functional Programming are of 3 Types
+// Pure Function , HOF , Currying Functions
 
-// // Example:-1
-// function sum(a){
-//      return (b) =>{
-//       return (c) =>{
-//          return (d) =>{
-//             return a+b+c+d ;
-//          }
-//       }  
-//      }
-// }
-// console.log(sum((10,20,30,40)))       //No Output
-// // this is the way , you can pass argument in  Currying Function
-// console.log(sum(10)(20)(30)(40)) ;
+// Normal Function
+function add(a,b,c,d){
+    return a+b+c+d ;
+}
+console.log(add(1,2,3,4)) ;     //10
 
 
-// // Example:-2
-// function power(a){
-//     return (b) =>{
-//      return (c) =>{
-//         return (d) =>{
-//            return a**b**c**d ;
-//         }
-//      }  
-//     }
-// }
-// // this is the way , you can pass argument in  Currying Function
-// console.log(power(2)(2)(1)(0)) ;      //4
+// Currying Function
 
-// // Example:-3
-// function sub(a){          //main(1) main(3)  main(6) a=10
-//     return (b) =>{       //b=2    b=4    b=" "
-//         if(b) return sub(a+b)     //main(3) main(6)  main(10)
-//         return a ;
-//     }
-// }
-// console.log(sub(1)(2)(3)(4)()) ;
+// Example:-1
+function sum(a){
+     return (b) =>{
+      return (c) =>{
+         return (d) =>{
+            return a+b+c+d ;
+         }
+      }  
+     }
+}
+console.log(sum((10,20,30,40)))       //No Output
+// this is the way , you can pass argument in  Currying Function
+console.log(sum(10)(20)(30)(40)) ;
 
-// // Example:-4
+
+// Example:-2
+function power(a){
+    return (b) =>{
+     return (c) =>{
+        return (d) =>{
+           return a**b**c**d ;
+        }
+     }  
+    }
+}
+// this is the way , you can pass argument in  Currying Function
+console.log(power(2)(2)(1)(0)) ;      //4
+
+// Example:-3
+function sub(a){          //main(1) main(3)  main(6) a=10
+    return (b) =>{       //b=2    b=4    b=" "
+        if(b) return sub(a+b)     //main(3) main(6)  main(10)
+        return a ;
+    }
+}
+console.log(sub(1)(2)(3)(4)()) ;
+
+// Example:-4
 let addition = function(a,b){
     return a+b ;
 }
@@ -67,7 +67,7 @@ let addition1 = addition.bind(this,1)
 console.log(addition1(3,5))       //4
 
 
-// // Example:-5
+// Example:-5
 let addition3 = function(a,b){
     return a+b ;
 }
@@ -75,7 +75,7 @@ let addition4 = addition3.bind(this,2,7)
 console.log(addition4(3,5))       //9
 
 
-// // Example:-6
+// Example:-6
 let addition5 = function(a,b,c,d){
     return a+b+c+d ;
 }
@@ -86,12 +86,12 @@ console.log(addition6(1,2))       //12
 // Currrying --  Currying is a process in functional programming  in which we can transform a function with multiple arguments into a sequence of nesting function . It returns a new function that excepts the next argument inline .
 
 
-// OR
+// //                                     OR
 
-// A function takes one argument at a time and return a new function expecting the next argument , then we will crete a new function which will take us another argument
+// // A function takes one argument at a time and return a new function expecting the next argument , then we will crete a new function which will take us another argument
 
 
-// NOTE- THE NUMBER OF ARGUMNETS A FUNCTION TAKES IS ALSO CALLED ARITY.
+// // NOTE- THE NUMBER OF ARGUMNETS A FUNCTION TAKES IS ALSO CALLED ARITY.
 //  here a , b is arity
 // function mul(a,b) {
 //     // code 
@@ -109,17 +109,17 @@ console.log(addition6(1,2))       //12
 // // or USING CURRY
 
 
-// function sum(a) {
-//     return function(b) {
-//         return function(c) {
-//             return a + b + c;
-//         }
-//     }
-// }
+function sum(a) {
+    return function(b) {
+        return function(c) {
+            return a + b + c;
+        }
+    }
+}
 
-// console.log(sum(2)(6)(3));
-// // console.log(sum(2)(6));
-// // console.log(sum(2));
+console.log(sum(2)(6)(3));
+console.log(sum(2)(6));        //it will not accept because one parameter is missing
+console.log(sum(2));            //it will not accept because Two parameter is missing
 
 
 // // Q2--
@@ -129,29 +129,20 @@ console.log(addition6(1,2))       //12
 // // evaluate("sub")(4)(2) => 2
 // // evaluate("divide")(4)(2) => 2
 
-// function evalute(operation) {
-//     return function(a) {
-//         return function(b) {
-//             if (operation === "sum") return a + b;
-//             else if (operation === "mul") return a * b;
-//             else if (operation === "divide") return a / b;
-//             else if (operation === "sub") return a - b;
-//             else return "Invalid Opertion";
-//         }
-//     }
-// }
+function evalute(operation) {
+    return function(a) {
+        return function(b) {
+            if (operation === "sum") return a + b;
+            else if (operation === "mul") return a * b;
+            else if (operation === "divide") return a / b;
+            else if (operation === "sub") return a - b;
+            else return "Invalid Opertion";
+        }
+    }
+}
 
-// console.log(evalute("sum")(4)(2));
+console.log(evalute("sum")(4)(2));
+console.log(evalute("mul")(4)(2));
+console.log(evalute("sub")(4)(2));
+console.log(evalute("divide")(4)(2));
 
-
-// //Q3 -- Infinite Currying -> sum(1)(2)(3).....(n)
-
-
-// function add(a) {
-//     return function(b) {
-//         if (b) return add(a + b);
-//         return a;
-//     };
-// }
-// console.log(add(2)(3)(4)());
-// console.log(add(2)(3)(4)(2));
