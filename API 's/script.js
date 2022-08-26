a53ec55ba731e5c27b1c520aa9b4d4c6eae4d7f1
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-26 12:17:44 +0530

    Fetching Text from another file

--- API 's/script.js
+++ API 's/script.js
@@ -1,11 +1,42 @@
 document.getElementById("btn").addEventListener("click",
 makeRequest) ;
 
+// function makeRequest(){
+//     console.log("Button Clicked") ;
+//     const promiseObj = fetch('data.txt') ;
+//     console.log(promiseObj) ;
+
+//     promiseObj
+//                .then((res) => {
+//                    console.log(res) ;
+//                    return res.text() ;
+//                })
+//                .then((data) => {
+//                console.log(data) ;
+//                })
+// }
+
+// //  OR we can also write this function
+
 function makeRequest(){
-    console.log("Button Clicked")
-    const promiseObj = fetch('data.txt')
-    console.log(promiseObj)
-    promiseObj.then((res) => {
-        console.log(res)
-    })
+    console.log("Button Clicked") ;
+
+    fetch('data.txt')
+                      .then((res) => {
+                          console.log(res) ;
+                          return res.text() ;
+                      })
+                      .then((data) => {
+                      console.log(data) ;
+                      })
 }
+
+
+
+
+
+
+
+
+
+
