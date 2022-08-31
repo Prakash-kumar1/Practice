54455e6e4fb9fc59e35a37e21d3dbbb8e89cdd2f
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-31 15:22:14 +0530

    Storage  and  Methods

--- 29August.js
+++ 29August.js
@@ -1,58 +1,58 @@
-// // Normal Function
-// function addNormal(n1 , n2){
-//     return n1+n2 ;
-// }
+// Normal Function
+function addNormal(n1 , n2){
+    return n1+n2 ;
+}
 
-// //Currying Function
-// function addCurry(n1){
-//     return function(n2){
-//         return n1+n2 ;
-//     } ;
-// }
+//Currying Function
+function addCurry(n1){
+    return function(n2){
+        return n1+n2 ;
+    } ;
+}
 
-// console.log(addNormal(10,5)) ;
+console.log(addNormal(10,5)) ;
 
-// console.log(addCurry(10)(5)) ;    //15
-// // OR
-// let sum = addCurry(20) ;
-// console.log(sum(30)) ;      //50
+console.log(addCurry(10)(5)) ;    //15
+// OR
+let sum = addCurry(20) ;
+console.log(sum(30)) ;      //50
 
 
-// // Comparing Normal Function  and Currying Function using Examples
+// Comparing Normal Function  and Currying Function using Examples
 
-// function validate(userName , password){
-//     originalUserName = "Prakash" ;    //username from DB => 1sec
-//     originalPassword = "123" ;    //Password from DB => 1sec
+function validate(userName , password){
+    originalUserName = "Prakash" ;    //username from DB => 1sec
+    originalPassword = "123" ;    //Password from DB => 1sec
 
-//     if(userName === originalUserName && password === originalPassword)
-//         return true ;
-//         return false ;
-// }
-// console.log(validate("Prakash" , "123"))      //true
-// // but when we give false value , returns false
-// console.log(validate("anjali" , "123"))      //false
+    if(userName === originalUserName && password === originalPassword)
+        return true ;
+        return false ;
+}
+console.log(validate("Prakash" , "123"))      //true
+// but when we give false value , returns false
+console.log(validate("anjali" , "123"))      //false
 
-// // in this case Ist it will check userName through DB in 1 sec , then it
-// // it find userName is wrong but it won't stop . it go and checks Password from DB in 1 sec
-// // this process taking 2 second .
+// in this case Ist it will check userName through DB in 1 sec , then it
+// it find userName is wrong but it won't stop . it go and checks Password from DB in 1 sec
+// this process taking 2 second .
 
-// // Currying Function for above example
+// Currying Function for above example
 
-// function validateUserName(username){
-//    originalUserName = "Govind Verma" ;    //username from DB => 1sec
-//    if(username === originalUserName){
-//     return function validatePassword(password){
-//         originalPassword = "123" ;  //Password from DB => 1sec
-//        if(password === originalPassword){
-//         return true ;
-//        } 
-//        return false ;
-//     } ;
-//    } 
-//    return false ;
-// }
+function validateUserName(username){
+   originalUserName = "Govind Verma" ;    //username from DB => 1sec
+   if(username === originalUserName){
+    return function validatePassword(password){
+        originalPassword = "123" ;  //Password from DB => 1sec
+       if(password === originalPassword){
+        return true ;
+       } 
+       return false ;
+    } ;
+   } 
+   return false ;
+}
 
-// console.log(validateUserName("Govind Verma") ("123"))      //true
+console.log(validateUserName("Govind Verma") ("123"))      //true
 
 // // if there is error , it will stop . don't go further
 // // console.log(validateUserName("Gayatri") ("123"))      //Error
@@ -75,6 +75,7 @@
 
 
 // Local Storage and  session storage
+
 // //  session storage=> data is persisted only for the perticular session , as soon as i claose the window, alll data will gone-->
 // //  local storage is same as session storage but the advantage is it doesnt clr itself, even if we close the tab or shut down laptop and again restart it the dtata is still persisted over there inn browser -->
 // // local storage have higher memory capacity than session storage 
