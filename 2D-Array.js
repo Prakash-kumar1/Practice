7244490c116472455d159c0c2e3910d4c8ddba20
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-12-31 15:04:59 +0530

    Update 2D-Array.js

---
+++ 2D-Array.js
@@ -0,0 +1,30 @@
+// Question no:-1 =>  what is 2d array in javascript ??
+// Answer =>  The two-dimensional array is a collection of items that share a common name and they are organized 
+//             as a matrix in the form of rows and columns. The two-dimensional array is an array of arrays, 
+//             so we create an array of one-dimensional array objects.
+//             2D arrays, commonly known as, matrices, are used in image processing. 
+//             It is also used in speech processing, in which each speech signal is an array.
+//             The syntax of two-dimensional arrays is: Data_type name_of the array[rows][index];
+
+
+// ###$$%%^^&&**-->>  Creating 2D array  ###$$%%^^&&**-->>
+
+// let arr = [ [], [], []]
+//  arr[0][0] = "Rahul" ;
+//  arr[0][1] = "Lenovo" ;
+//  arr[0][2] = 300 ;
+
+//  arr[1][0] = "Sonam" ;
+//  arr[1][1] = "MacBook" ;
+//  arr[1][2] = 500 ;
+
+//  arr[2][0] = "Dinesh" ;
+//  arr[2][1] = "Hp" ;
+//  arr[2][2] = 700 ;
+
+//  arr[3][0] = "Kartik" ;
+//  arr[3][1] = "Dell" ;
+//  arr[3][2] = 900 ;
+
+
+let arr = [ [], [], []]
