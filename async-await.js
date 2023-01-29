f17720d00e6b8b5ff705ae8da7c718fff61807a5
Prakash-kumar1 <kumarsanju4881@gmail.com>
2023-01-29 22:17:28 +0530

    Update async-await.js

--- async-await.js
+++ async-await.js
@@ -6,7 +6,6 @@
 // https://www.javascripttutorial.net/javascript-try-catch/
 
 
-
 // Async: It simply allows us to write promises-based code as if it was synchronous and it checks that we are not breaking the execution 
 //        thread. It operates asynchronously via the event loop. Async functions will always return a value. It makes sure that
 //  a promise is returned and if it is not returned then JavaScript automatically wraps it in a promise which is resolved with its value.
