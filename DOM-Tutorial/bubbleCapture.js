40d06cef36baf25d15aa523073f6f86f744ddccd
Prakash-kumar1 <kumarsanju4881@gmail.com>
2023-02-08 20:32:54 +0530

    Event Bubbling | Capturing | delegation

--- DOM-Tutorial/bubbleCapture.js
+++ DOM-Tutorial/bubbleCapture.js
@@ -7,28 +7,28 @@ const grandParent = document.querySelector(".grandparent") ;
 const Parent = document.querySelector(".parent") ;
 const child = document.querySelector(".child") ;
 
-// child.addEventListener("click", ()=>{
-//     console.log("you clicked on child") ;
-// })
+child.addEventListener("click", ()=>{
+    console.log("you clicked on child") ;
+})
 
-// Parent.addEventListener("click", ()=>{
-//     console.log("you clicked on parent") ;
-// })
+Parent.addEventListener("click", ()=>{
+    console.log("you clicked on parent") ;
+})
 
-// grandParent.addEventListener("click", ()=>{
-//     console.log("you clicked on grandparent") ;
-// })
+grandParent.addEventListener("click", ()=>{
+    console.log("you clicked on grandparent") ;
+})
 
-// document.body.addEventListener("click", ()=>{
-//     console.log("you clicked on document.body") ;
-// })
+document.body.addEventListener("click", ()=>{
+    console.log("you clicked on document.body") ;
+})
 
 
 
-// // Question no:-1 =>>  what is Event Capturing in JavaScript ??
-// // Answer =>> Event capturing is one of two ways to do event propagation in the HTML DOM. In event capturing, an event propagates 
-// //            from the outermost element  to the target element. Capturing happens before bubbling.
-// //             It is the opposite of event bubbling, where events propagate outwards from the target to the outer elements.
+// Question no:-1 =>>  what is Event Capturing/Trickling in JavaScript ??
+// Answer =>> Event capturing/Trickling is one of two ways to do event propagation in the HTML DOM. In event capturing, an event propagates 
+//            from the outermost element  to the target element. Capturing happens before bubbling.
+//             It is the opposite of event bubbling, where events propagate outwards from the target to the outer elements.
 
 
 // child.addEventListener("click", ()=>{
@@ -47,32 +47,55 @@ const child = document.querySelector(".child") ;
 //     console.log("Capturing events on document.body") ;
 // }, true) ;
 
-
+// NOTE ->>  For stopping bubbling & capturing, we used e.stopPropogation()
 
 // Question no:-1 =>>  what is Event Delegation in JavaScript ??
 // Answer =>> Event Delegation is a pattern based upon the concept of Event Bubbling. It is an event-handling  pattern that
 //       allows you to handle events at a higher level in the DOM tree other than the level where the event was first received.
 
-grandParent.addEventListener("click", (e) => {
-    console.log(e);
-  console.log(e.target);
-});
+// grandParent.addEventListener("click", (e) => {
+//     console.log(e);
+//   console.log(e.target);
+// });
+
+// !!!!!!!!@@@@@@@@@##############$$$$$$$$$$$$$$$$%%%%%%%%%%^^^^^^^^^^^^&&&&&&&&&&&&***************+++++++++++++++____________+++++++????????????????::::::::]]]]]]]]{{{{{{{{{}}}}}}}}}
+
+// Question no-1: " event.target "  vs  " this.target  "   vs   " event.currentTarget " 
+
+const div = document.querySelector("div") ;
+const form = document.querySelector("form") ;
+const button = document.querySelector("button") ;
+
+div.addEventListener("click", func) ;
+button.addEventListener("click", func) ;
+form.addEventListener("click", func) ;
+
+// event.currentTarget and this.target gives bubbling, But event.target sticks to its origin
+function func(event){
+  alert("currentTarget = " + event.currentTarget.tagName +
+    ", target = " +  event.target.tagName + 
+    ", this = " + this.tagName 
+  ) ;
+}
 
 
+// Question no-2: Create a Modal which closes by clicking on negative space ??
+const btn = document.querySelector(".modalButton") ;
+const container = document.querySelector(".modalContainer") ;
 
+btn.addEventListener("click", () => {
+  toggleModal(true) ;
+}) ;
 
+function toggleModal(toggle){
+  container.style.display = toggle ? "flex" : "none"
+}
 
-
-
-
-
-
-
-
-
-
-
-
+container.addEventListener("click", (e) => {
+  toggleModal(false) ;
+// For clicking on outer area
+// if(e.target.className === "modalContainer")      toggleModal(false) ;
+}) ;
 
 
 
